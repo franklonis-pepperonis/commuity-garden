67a001806ce42e6775cdb96581bca350af30249b
Grae Abbott <highsierra@Graes-MacBook-Pro-3.local>
2020-03-20 21:24:13 -0500

    modifications to collection view

---
+++ CommunityGarden/CommunityGarden/YourCollectionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  YourCollectionViewController.swift
+//  CommunityGarden
+//
+//  Created by Grae Abbott on 3/20/20.
+//  Copyright © 2020 FrankPepps. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class YourCollectionViewController: UIViewController{
+    override func viewDidLoad(){
+        super.viewDidLoad();
+    }
+}
