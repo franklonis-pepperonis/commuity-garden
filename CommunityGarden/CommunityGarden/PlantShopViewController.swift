0ba598030e99330ebcadf648476559146184417c
Olivia Sun <oliviasun88@gmail.com>
2020-03-21 16:16:33 -0400

    changed name of profile view, added gitignore

---
+++ CommunityGarden/CommunityGarden/PlantShopViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PlantShopViewController.swift
+//  CommunityGarden
+//
+//  Created by Olivia Sun on 3/21/20.
+//  Copyright © 2020 FrankPepps. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PlantShopViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+}
