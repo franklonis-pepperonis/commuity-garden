8b9b8a574fee344e7de45f40e2b12f4d7ce5ca5e
fraluo <fraluo@umich.edu>
2020-04-06 17:04:38 -0400

    login/sign up pages

---
+++ CommunityGarden/CommunityGarden/SignupViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignupViewController.swift
+//  CommunityGarden
+//
+//  Created by Franklin Luo on 4/6/20.
+//  Copyright © 2020 FrankPepps. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SignupViewController: UIViewController{
+    override func viewDidLoad(){
+        super.viewDidLoad();
+    }
+}
