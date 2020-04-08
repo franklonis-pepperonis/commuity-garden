e38fd90a4b109942820f9a027f1efd7c761b5491
fraluo <fraluo@umich.edu>
2020-04-08 14:28:54 -0400

    garden icons

--- CommunityGarden/CommunityGarden/MapViewController.swift
+++ CommunityGarden/CommunityGarden/MapViewController.swift
@@ -40,7 +40,9 @@ class MapViewController: UIViewController {
         
         // eventually we need to change these to gardens and each AR item should really be a dof
         let firstPlant = ARItem(itemDescription: "Plant", location: CLLocation(latitude:42.2716, longitude:-83.7464), itemNode: nil)
+        let secondPlant = ARItem(itemDescription: "Plant 2", location: CLLocation(latitude: 42.271740, longitude: -83.740227), itemNode: nil)
         plants.append(firstPlant)
+        plants.append(secondPlant)
         
         for item in plants {
             let annotation = MapAnnotation(location: item.location.coordinate, item: item)
@@ -53,7 +55,18 @@ extension MapViewController: MKMapViewDelegate {
   func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
     self.userLocation = userLocation.location
   }
-    
+
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+
+        else {
+            let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "annotationView") ?? MKAnnotationView()
+            annotationView.image = UIImage(named: "garden")
+            return annotationView
+        }
+    }
   func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
 
     let coordinate = view.annotation!.coordinate
