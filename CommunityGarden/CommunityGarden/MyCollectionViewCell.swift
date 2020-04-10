e34bf226a2b905deac45c04cbfa17e6d4cd627b8
Sri Gulukota <srig@umich.edu>
2020-04-10 16:22:50 -0400

    manually combined grae and sri's code

---
+++ CommunityGarden/CommunityGarden/MyCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyCollectionViewCell.swift
+//  CommunityGarden
+//
+//  Created by Grae Abbott on 4/9/20.
+//  Copyright © 2020 FrankPepps. All rights reserved.
+//
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+     @IBOutlet weak var myLabel: UILabel!
+}
