52577221497faf4a99392cb8d0b458529d3f3a54
Sri Gulukota <srig@umich.edu>
2020-04-10 18:33:41 -0400

    re-did signup, login, profile

--- CommunityGarden/CommunityGarden/LoginViewController.swift
+++ CommunityGarden/CommunityGarden/LoginViewController.swift
@@ -5,46 +5,78 @@
 //  Created by Franklin Luo on 4/6/20.
 //  Copyright © 2020 FrankPepps. All rights reserved.
 //
-
 import Foundation
 import UIKit
 import Firebase
 import FirebaseFirestore
 
 class LoginViewController: UIViewController{
-    @IBOutlet weak var username: UITextView!
     
-    @IBOutlet weak var password: UITextView!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
     
     override func viewDidLoad(){
         super.viewDidLoad();
     }
     
     @IBAction func ValidateLogin(_ sender: UIButton) {
-        
-        print(self.username.text, self.password.text)
-        
         // Check if valid username and password
+        let username = self.username.text as! String
+        let password = self.password.text as! String
+        self.validateUser(username: username, password: password) { (isValid) in
+            if isValid {
+                // set cur_user efor this session
+                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                appDelegate.cur_user = self.username.text
+                // it exists, continue to map view
+                let mapView =  self.storyboard?.instantiateViewController(withIdentifier: "MapViewController") as! MapViewController
+                self.present(mapView, animated: true, completion: nil)
+            }
+            if !isValid {
+                // clear username and password fiels
+                self.username.text = ""
+                self.password.text = ""
+                // user does not exist, send alert
+                let alert = UIAlertController(title: "Incorrect Username or Password", message: "Retry log in", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                alert.addAction(UIAlertAction(title: "Sign Up", style: .default, handler: self.signUpHandler))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    };
+    
+    func signUpHandler(action: UIAlertAction) {
+        let signUpView =  self.storyboard?.instantiateViewController(withIdentifier: "SignupViewController") as! SignupViewController
+        self.present(signUpView, animated: true, completion: nil)
+    };
+    
+    
+    func validateUser(username: String, password: String, completion: @escaping (Bool) -> Void) {
         let db = Firestore.firestore()
         db.collection("users").getDocuments() { (querySnapshot, err) in
             if let err = err {
                 print("Error getting documents: \(err)")
+                completion(false)
             } else {
                 for document in querySnapshot!.documents {
+                    // check for valid username
                     if document.documentID == self.username.text {
                         let password = document.data()["password"] as? String
-                        print(password)
+                        // check for correct password
                         if  password == self.password.text {
-                            print("\(document.data())")
+                            completion(true)
+                            return
+                        } else {
+                            completion(false)
+                            return
                         }
                     }
                 }
+                completion(false)
+                return
             }
         }
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        appDelegate.cur_user = self.username.text
-        print(appDelegate.cur_user)
-        
-    }
+    };
     
 }
--- CommunityGarden/CommunityGarden/SignupViewController.swift
+++ CommunityGarden/CommunityGarden/SignupViewController.swift
@@ -5,12 +5,89 @@
 //  Created by Franklin Luo on 4/6/20.
 //  Copyright © 2020 FrankPepps. All rights reserved.
 //
-
 import Foundation
 import UIKit
+import FirebaseFirestore
 
 class SignupViewController: UIViewController{
+
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var retypedPassword: UITextField!
+    
+    
     override func viewDidLoad(){
         super.viewDidLoad();
     }
+
+    @IBAction func signUp(_ sender: Any) {
+        // check that username doesn't already exist
+        let username = self.username.text as! String
+        self.validateUsername(username: username) { (isValid) in
+            if !isValid {
+                print("not valid")
+                // clear username field
+                self.username.text = ""
+                // alert that they need to pick a new username
+                let alert = UIAlertController(title: "Username is taken", message: "Try new username", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                print("valid")
+                // check that passwords match too
+                if self.password.text == self.retypedPassword.text {
+                    print("password match")
+                    // set cur_user for this session
+                    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                    appDelegate.cur_user = self.username.text
+                    // add data to database
+                    let password = self.password.text as! String
+                    let firstName = self.firstName.text as! String
+                    let lastName = self.lastName.text as! String
+                    self.addUser(username: username, password: password, firstName: firstName, lastName: lastName)
+                    // go to mapView
+                    let mapView =  self.storyboard?.instantiateViewController(withIdentifier: "MapViewController") as! MapViewController
+                    self.present(mapView, animated: true, completion: nil)
+                } else {
+                    // clear password fields
+                    self.password.text = ""
+                    self.retypedPassword.text = ""
+                    // send alert message for not matching passwords
+                    print("printing alert")
+                    let alert = UIAlertController(title: "Retried password doesn't match password", message: "Re-type passwords", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    func validateUsername(username: String, completion: @escaping (Bool) -> Void) {
+        print("validating username")
+        let db = Firestore.firestore()
+        db.collection("users").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print("ID \(document.documentID)")
+                    if document.documentID == username {
+                        completion(false)
+                        return
+                    }
+                }
+            }
+            completion(true)
+            return
+        }
+    };
+    
+    func addUser(username: String, password: String, firstName: String, lastName: String) {
+        let db = Firestore.firestore();
+        let data = ["first_name": firstName, "last_name": lastName, "password": password, "coins": 50, "water_available": 100] as [String: Any]
+        db.collection("users").document(username).setData(data);
+    }
+
 }
