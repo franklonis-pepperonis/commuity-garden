680af59725ceab537a544399c8343ed36c384430
nikhilbh <nikhilbh@umich.edu>
2020-04-11 13:14:50 -0400

    changes

--- CommunityGarden/CommunityGarden/PlantPlotViewController.swift
+++ CommunityGarden/CommunityGarden/PlantPlotViewController.swift
@@ -31,7 +31,7 @@ class PlantPlotViewController: UIViewController
     
     // should eventually hold plant info to display
     var plant_id : String?
-    
+    var plant_ar : String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,58 +45,62 @@ class PlantPlotViewController: UIViewController
             } else {
                 self.PlantName.text = Plant!.data()!["name"] as? String
                 self.PlantOwner.text = Plant!.data()!["owner"] as? String
-                self.PlantHealth.text = Plant!.data()!["health"] as? String
+                let health = Plant!.data()!["health"]
+                self.PlantHealth.text = "\(health!)"
+                print("hello!")
+                print(Plant!.data()!)
+                
+                
+                self.plant_ar = "art.scnassets/" + "\(self.PlantName.text!)" + ".dae"
+               
+                print(self.plant_ar!)
+               
+                // change to image pulled from database from plant.image
+                let scene = SCNScene(named: self.plant_ar!)
+               
+                // Add camera node
+                let cameraNode = SCNNode()
+                cameraNode.camera = SCNCamera()
+                cameraNode.position = SCNVector3(x: 0, y: 0, z: 5)
+                scene?.rootNode.addChildNode(cameraNode)
+               
+               
+                // Add custom lighting
+                /*
+                 let lightNode = SCNNode()
+                 lightNode.light = SCNLight()
+                 lightNode.light?.type = .omni
+                 lightNode.position = SCNVector3(x: 0, y: 10, z: 35)
+                 scene?.rootNode.addChildNode(lightNode)
+               
+                 let ambientLightNode = SCNNode()
+                 ambientLightNode.light = SCNLight()
+                 ambientLightNode.light?.type = .ambient
+                 ambientLightNode.light?.color = UIColor.darkGray
+                 scene?.rootNode.addChildNode(ambientLightNode)
+                 */
+        
+                // If you don't want to fix manually the lights
+                self.sceneView.autoenablesDefaultLighting = true
+               
+                // Allow user to manipulate camera
+                self.sceneView.allowsCameraControl = true
+               
+                // Show FPS logs and timming
+                // sceneView.showsStatistics = true
+               
+                // Set background color
+                self.sceneView.backgroundColor = UIColor.white
+               
+                // Allow user translate image
+                self.sceneView.cameraControlConfiguration.allowsTranslation = false
+               
+                // Set scene settings
+                self.sceneView.scene = scene
+                
             }
         }
-        
-        
-        
-        // change to image pulled from database from plant.image
-        let scene = SCNScene(named: "art.scnassets/\(String(describing: self.PlantName.text)).dae")
-        
-        // Add camera node
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        cameraNode.position = SCNVector3(x: 0, y: 10, z: 35)
-        scene?.rootNode.addChildNode(cameraNode)
-        
-        
-        // Add custom lighting
-        /*
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light?.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 35)
-        scene?.rootNode.addChildNode(lightNode)
-        
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light?.type = .ambient
-        ambientLightNode.light?.color = UIColor.darkGray
-        scene?.rootNode.addChildNode(ambientLightNode)
-        */
- 
-        // If you don't want to fix manually the lights
-        sceneView.autoenablesDefaultLighting = true
-        
-        // Allow user to manipulate camera
-        sceneView.allowsCameraControl = true
-        
-        // Show FPS logs and timming
-        // sceneView.showsStatistics = true
-        
-        // Set background color
-        sceneView.backgroundColor = UIColor.white
-        
-        // Allow user translate image
-        sceneView.cameraControlConfiguration.allowsTranslation = false
-        
-        // Set scene settings
-        sceneView.scene = scene
-        
-        
     }
-    
 }
 
 
