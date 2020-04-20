43d0cdfcc7ea3c121a0be8550e740e3a5baa509e
nikhilbh <nikhilbh@umich.edu>
2020-04-20 16:23:48 -0400

    plant health and user refresh

--- CommunityGarden/CommunityGarden/AppDelegate.swift
+++ CommunityGarden/CommunityGarden/AppDelegate.swift
@@ -24,6 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         FirebaseApp.configure()
         
+        self.updateDatabase()
+        
         return true
     }
     
@@ -50,5 +52,84 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     
+    func updateDatabase(){
+        let db = Firestore.firestore()
+        
+        let docRef = db.collection("update").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("\n")
+                print("Error getting documents: \(err)")
+                print("\n")
+            } else{
+                for document in querySnapshot!.documents{
+
+                    let last_timestamp = document.data()["last_update"] as? Timestamp
+                    let then = last_timestamp?.dateValue()
+                    let now: NSDate! = NSDate()
+                    
+                    let calendar = Calendar.current
+
+                    let date1 = calendar.startOfDay(for: then!)
+                    let date2 = calendar.startOfDay(for: (now! as Date))
+
+                    let components = calendar.dateComponents([.day], from: date1, to: date2)
+                    
+                    if(components.day! > 0){
+                        // if days have passed, update needs update value to true
+                        db.collection("update").document("needs_update").setData(["last_update" : now!], merge: true)
+                        
+                        
+                    //}
+                    //if ((document.data()["value"]) as? Bool == true){
+                        //iterate through all plants and decrement by value
+                        db.collection("plant IDs").getDocuments() { (allPlants, err) in
+                            if let err = err {
+                                print("Error getting plants: \(err)")
+                            } else {
+                                for plant in allPlants!.documents {
+                                    var curHealth = plant.data()["health"] as! Int
+                                    curHealth -= 10
+                                    var plant_id = plant.documentID as String
+                                    if (curHealth <= 0){
+                                        let plant_owner = plant.data()["owner"] as! String
+                                        let plant_garden = plant.data()["garden"] as! String
+                                        //remove plant
+                                        db.collection("users").document(plant_owner).collection("planted").document(plant_id).delete()
+                                                        
+                                        // remove plant from plants database
+                                        db.collection("plant IDs").document(plant_id).delete()
+                                        
+                                        // remove plant_id from garden database
+                                        db.collection("gardens").document(plant_garden).collection("plants").document(plant_id).delete()
+                                    } else{
+                                        db.collection("plant IDs").document(plant_id).setData(["health": curHealth], merge: true)
+                                    }
+                                }
+                            }
+                        }
+                        
+                        
+                        //iterate through all users and set water level to 100
+                        //iterate through all users and give them 5 coins
+                        db.collection("users").getDocuments() { (users, err) in
+                            if let err = err {
+                                print("Error loading users: \(err)")
+                            } else {
+                                for user in users!.documents {
+                                    let user_id = user.documentID
+                                    let userWater = 100
+                                    var curCoinCount = user.data()["coins"] as! Int
+                                    curCoinCount += 5
+                                    db.collection("users").document(user_id).setData(["water_available": userWater], merge: true)
+                                    db.collection("users").document(user_id).setData(["coins": curCoinCount], merge: true)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
+
 
